49b3306072dc6cca8c00776017dfd81d027cf1e9
joaovictorgermoliato <joaovictorgermoliato@uni9.edu.br>
2024-05-28 02:20:00 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo Genius-Simon/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Jogo_Genius_Simon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btCiano = new Button();
+            btRoxo = new Button();
+            btIniciar = new Button();
+            lbNivel = new Label();
+            label1 = new Label();
+            btAzul = new Button();
+            btVermelho = new Button();
+            btAmarelo = new Button();
+            btVerde = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            btLaranja = new Button();
+            btRosa = new Button();
+            SuspendLayout();
+            // 
+            // btCiano
+            // 
+            btCiano.FlatStyle = FlatStyle.Flat;
+            btCiano.Location = new Point(401, 215);
+            btCiano.Name = "btCiano";
+            btCiano.Size = new Size(132, 125);
+            btCiano.TabIndex = 17;
+            btCiano.UseVisualStyleBackColor = true;
+            btCiano.Click += bt_Click;
+            // 
+            // btRoxo
+            // 
+            btRoxo.BackColor = SystemColors.Control;
+            btRoxo.FlatStyle = FlatStyle.Flat;
+            btRoxo.Location = new Point(401, 70);
+            btRoxo.Name = "btRoxo";
+            btRoxo.Size = new Size(132, 127);
+            btRoxo.TabIndex = 16;
+            btRoxo.UseVisualStyleBackColor = false;
+            btRoxo.Click += bt_Click;
+            // 
+            // btIniciar
+            // 
+            btIniciar.Font = new Font("Segoe UI", 16F);
+            btIniciar.Location = new Point(324, 443);
+            btIniciar.Name = "btIniciar";
+            btIniciar.Size = new Size(108, 50);
+            btIniciar.TabIndex = 15;
+            btIniciar.Text = "Iniciar";
+            btIniciar.UseVisualStyleBackColor = true;
+            btIniciar.Click += btIniciar_Click;
+            // 
+            // lbNivel
+            // 
+            lbNivel.AutoSize = true;
+            lbNivel.Font = new Font("Segoe UI", 20F);
+            lbNivel.Location = new Point(379, 368);
+            lbNivel.Name = "lbNivel";
+            lbNivel.Size = new Size(0, 37);
+            lbNivel.TabIndex = 14;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(243, 368);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 37);
+            label1.TabIndex = 13;
+            label1.Text = "Nível :";
+            label1.Click += label1_Click;
+            // 
+            // btAzul
+            // 
+            btAzul.FlatStyle = FlatStyle.Flat;
+            btAzul.Location = new Point(250, 215);
+            btAzul.Name = "btAzul";
+            btAzul.Size = new Size(132, 125);
+            btAzul.TabIndex = 12;
+            btAzul.UseVisualStyleBackColor = true;
+            btAzul.Click += bt_Click;
+            // 
+            // btVermelho
+            // 
+            btVermelho.FlatStyle = FlatStyle.Flat;
+            btVermelho.Location = new Point(105, 215);
+            btVermelho.Name = "btVermelho";
+            btVermelho.Size = new Size(128, 125);
+            btVermelho.TabIndex = 11;
+            btVermelho.UseVisualStyleBackColor = true;
+            btVermelho.Click += bt_Click;
+            // 
+            // btAmarelo
+            // 
+            btAmarelo.BackColor = SystemColors.Control;
+            btAmarelo.FlatStyle = FlatStyle.Flat;
+            btAmarelo.Location = new Point(250, 70);
+            btAmarelo.Name = "btAmarelo";
+            btAmarelo.Size = new Size(132, 127);
+            btAmarelo.TabIndex = 10;
+            btAmarelo.UseVisualStyleBackColor = false;
+            btAmarelo.Click += bt_Click;
+            // 
+            // btVerde
+            // 
+            btVerde.FlatStyle = FlatStyle.Flat;
+            btVerde.Location = new Point(105, 70);
+            btVerde.Name = "btVerde";
+            btVerde.Size = new Size(130, 127);
+            btVerde.TabIndex = 9;
+            btVerde.UseVisualStyleBackColor = true;
+            btVerde.Click += bt_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 250;
+            timer1.Tick += timer1_Tick;
+            // 
+            // btLaranja
+            // 
+            btLaranja.FlatStyle = FlatStyle.Flat;
+            btLaranja.Location = new Point(554, 215);
+            btLaranja.Name = "btLaranja";
+            btLaranja.Size = new Size(132, 125);
+            btLaranja.TabIndex = 19;
+            btLaranja.UseVisualStyleBackColor = true;
+            btLaranja.Click += bt_Click;
+            // 
+            // btRosa
+            // 
+            btRosa.BackColor = SystemColors.Control;
+            btRosa.FlatStyle = FlatStyle.Flat;
+            btRosa.Location = new Point(554, 70);
+            btRosa.Name = "btRosa";
+            btRosa.Size = new Size(132, 127);
+            btRosa.TabIndex = 18;
+            btRosa.UseVisualStyleBackColor = false;
+            btRosa.Click += bt_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 556);
+            Controls.Add(btLaranja);
+            Controls.Add(btRosa);
+            Controls.Add(btCiano);
+            Controls.Add(btRoxo);
+            Controls.Add(btIniciar);
+            Controls.Add(lbNivel);
+            Controls.Add(label1);
+            Controls.Add(btAzul);
+            Controls.Add(btVermelho);
+            Controls.Add(btAmarelo);
+            Controls.Add(btVerde);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btCiano;
+        private Button btRoxo;
+        private Button btIniciar;
+        private Label lbNivel;
+        private Label label1;
+        private Button btAzul;
+        private Button btVermelho;
+        private Button btAmarelo;
+        private Button btVerde;
+        private System.Windows.Forms.Timer timer1;
+        private Button btLaranja;
+        private Button btRosa;
+    }
+}
---
+++ Jogo Genius-Simon/Form1.cs
@@ -0,0 +1,279 @@
+using System.Drawing;
+using System.Media;
+
+namespace Jogo_Genius_Simon
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        int sequenciaSelecionada = -1;
+        int sequenciaAtual = 0;
+        int aux = 0;
+        List<Button> sequencia;
+        Color corPadrao;
+        Color corPiscando;
+        bool piscando = false;
+        int nivel = 0;
+
+        //Cor Padrao/Inicial
+        string corVerdePadrao = "DarkGreen";
+        string corAmareloPadrao = "192; 192; 0";
+        string corVermelhoPadrao = "DarkRed";
+        string corAzulPadrao = "DarkBlue";
+        string corCianoPadrao = "#17A589";
+        string corRoxoPadrao = "#8E44AD";
+
+        string corRosaPadrao = "#fc417c";
+        string corLaranjaPadrao = "#e13600";
+
+
+        //Pisacando cor, Quando piscar troca para essa cor
+        string corVerdePiscando = "LightGreen";
+        string corAmareloPiscando = "#FFFF99";
+        string corVermelhoPiscando = "#F50E00";
+        string corAzulPiscando = "LightBlue";
+        string corCianoPiscando = "#A3E4D7";
+        string corRoxoPiscando = "#D2B4DE";
+
+        string corRosaPiscando = "#fda0bd";
+        string corLaranjaPiscando = "#fdc4b2";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btVerde.BackColor = ColorTranslator.FromHtml(corVerdePadrao);
+            btAmarelo.BackColor = ColorTranslator.FromHtml(corAmareloPadrao);
+            btVermelho.BackColor = ColorTranslator.FromHtml(corVermelhoPadrao);
+            btAzul.BackColor = ColorTranslator.FromHtml(corAzulPadrao);
+            btCiano.BackColor = ColorTranslator.FromHtml(corCianoPadrao);
+            btRoxo.BackColor = ColorTranslator.FromHtml(corRoxoPadrao);
+
+            btRosa.BackColor = ColorTranslator.FromHtml(corRosaPadrao);
+            btLaranja.BackColor = ColorTranslator.FromHtml(corLaranjaPadrao);
+
+
+            btVerde.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corVerdePadrao);
+            btAmarelo.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corAmareloPadrao);
+            btVermelho.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corVermelhoPadrao);
+            btAzul.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corAzulPadrao);
+            btCiano.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corCianoPadrao);
+            btRoxo.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corRoxoPadrao);
+
+            btRosa.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corRosaPadrao);
+            btLaranja.FlatAppearance.MouseOverBackColor = ColorTranslator.FromHtml(corLaranjaPadrao);
+
+
+            btVerde.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corVerdePiscando);
+            btAmarelo.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corAmareloPiscando);
+            btVermelho.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corVermelhoPiscando);
+            btAzul.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corAzulPiscando);
+            btCiano.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corCianoPiscando);
+            btRoxo.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corRoxoPiscando);
+
+            btRosa.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corRosaPiscando);
+            btLaranja.FlatAppearance.MouseDownBackColor = ColorTranslator.FromHtml(corLaranjaPiscando);
+
+
+            btVerde.Cursor = Cursors.Hand;
+            btAmarelo.Cursor = Cursors.Hand;
+            btVermelho.Cursor = Cursors.Hand;
+            btAzul.Cursor = Cursors.Hand;
+            btCiano.Cursor = Cursors.Hand;
+            btRoxo.Cursor = Cursors.Hand;
+
+            btRosa.Cursor = Cursors.Hand;
+            btLaranja.Cursor = Cursors.Hand;
+
+
+            btVerde.Enabled = false;
+            btAmarelo.Enabled = false;
+            btVermelho.Enabled = false;
+            btAzul.Enabled = false;
+            btCiano.Enabled = false;
+            btRoxo.Enabled = false;
+
+            btRosa.Enabled = false;
+            btLaranja.Enabled = false;
+        }
+
+
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+            sequenciaSelecionada = -1;
+            sequencia = new List<Button>();
+            btIniciar.Enabled = false;
+
+            nivel++;
+
+            lbNivel.Text = nivel.ToString();
+
+            GerarSequencia();
+            Piscando();
+            HabilitaDesabilitaBotao(true);
+
+        }
+
+        private void GerarSequencia()
+        {
+            Random random = new Random();
+            int num = random.Next(0, 8);
+
+            Button bt = null;
+
+            switch (num)
+            {
+
+                case 0:
+                    bt = btVerde;
+                    break;
+                case 1:
+                    bt = btAmarelo;
+                    break;
+                case 2:
+                    bt = btVermelho;
+                    break;
+                case 3:
+                    bt = btAzul;
+                    break;
+                case 4:
+                    bt = btCiano;
+                    break;
+                case 5:
+                    bt = btRoxo;
+                    break;
+                case 6:
+                    bt = btRosa;
+                    break;
+                case 7:
+                    bt = btLaranja;
+                    break;
+
+
+            }
+
+            sequencia.Add(bt);
+        }
+    
+
+
+        private void Piscando()
+        {
+            aux = 0;
+
+            for (int i = 0; i < sequencia.Count; i++)
+            {
+                sequenciaAtual = i;
+
+                corPadrao = sequencia[i].BackColor;
+
+                if (sequencia[i] == btVerde)
+                    corPiscando = ColorTranslator.FromHtml(corVerdePiscando);
+                else if (sequencia[i] == btAmarelo)
+                    corPiscando = ColorTranslator.FromHtml(corAmareloPiscando);
+                else if (sequencia[i] == btVermelho)
+                    corPiscando = ColorTranslator.FromHtml(corVermelhoPiscando);
+                else if (sequencia[i] == btAzul)
+                    corPiscando = ColorTranslator.FromHtml(corAzulPiscando);
+                else if (sequencia[i] == btCiano)
+                    corPiscando = ColorTranslator.FromHtml(corCianoPiscando);
+                else if (sequencia[i] == btRoxo)
+                    corPiscando = ColorTranslator.FromHtml(corRoxoPiscando);
+                else if (sequencia[i] == btRosa)
+                    corPiscando = ColorTranslator.FromHtml(corRosaPiscando);
+                else if (sequencia[i] == btLaranja)
+                    corPiscando = ColorTranslator.FromHtml(corLaranjaPiscando);
+
+                timer1.Start();
+
+                while (aux == i)
+                {
+                    Application.DoEvents();
+                }
+
+            }
+
+        }
+
+        private void HabilitaDesabilitaBotao(bool habilita)
+        {
+            btVerde.Enabled = habilita;
+            btAmarelo.Enabled = habilita;
+            btVermelho.Enabled = habilita;
+            btAzul.Enabled = habilita;
+            btCiano.Enabled = habilita;
+            btRoxo.Enabled = habilita;
+            btRosa.Enabled = habilita;
+            btLaranja.Enabled = habilita;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!piscando)
+            {
+                sequencia[sequenciaAtual].BackColor = corPiscando;
+                piscando = true;
+            }
+            else
+            {
+                sequencia[sequenciaAtual].BackColor = corPadrao;
+                piscando = false;
+                aux++;
+                timer1.Stop();
+            }
+
+        }
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+            sequenciaSelecionada++;
+
+            if (sequencia[sequenciaSelecionada] == (Button)sender)
+            {
+                if (sequencia.Count - 1 == sequenciaSelecionada)
+                {
+                    nivel++;
+                    lbNivel.Text = nivel.ToString();
+                    sequenciaSelecionada = -1;
+                    HabilitaDesabilitaBotao(false);
+                    GerarSequencia();
+                    Thread.Sleep(500);
+                    Piscando();
+                    HabilitaDesabilitaBotao(true);
+
+                }
+            }
+
+            else
+            {
+                btIniciar.Enabled = true;
+                HabilitaDesabilitaBotao(false);
+                nivel = 0;
+                lbNivel.Text = "";
+                MessageBox.Show("Você errou a sequência! Fim de jogo!");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.Black;
+            this.Text = "Jogo Genius";
+            label1.ForeColor = Color.White;
+            lbNivel.ForeColor = Color.White;
+            btIniciar.BackColor = Color.LightGreen;
+            btIniciar.ForeColor = Color.Black;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
